ae93c88f304144214a3bdfbda640c6740e87f01f
sharadalt <ltsharada@gmail.com>
2016-05-10 17:42:33 +0000

    API_Controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
   
   get 'welcome/about'
   
+  namespace :api, defaults: { format: :json } do
+    
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+    
+  end
+  
+  
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
